f7685f37e1768914d750bc05bec1309a06f6f9a9
Matthew Cataldi <mcataloe@gmail.com>
2017-08-21 15:14:11 -0500

    Initial scaffolding and experimentation with git stash as a way to bring in changes that have had a pull request.

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -6,3 +6,36 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+// test with a normal callback
+test('the data is peanut butter', done => {
+  function callback(data){
+    expect(data).toBe('peanut butter');
+    done();
+  }
+
+  fetchData(callback);
+})
+
+
+
+// Promises
+test('the data is peanut butter', () => {
+  expect(assertions(1));
+
+  return fetchData().then(data => {
+    expect(data).toBe('peanut butter');
+  });
+
+});
+
+test("the fetch fails with an error", () => {
+  expect.assertions(1);
+
+  return fetchData().catch(e =>
+    expect(e).toMatch('error')
+  );
+});
+
+
+

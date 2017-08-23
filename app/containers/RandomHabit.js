5126d110ee854d6b05fca25bd36991eb8b0291aa
Mark Franciose <mark.franciose@gmail.com>
2017-08-23 13:38:40 -0500

    create random habit component for newhabit view

--- app/containers/RandomHabit.js
+++ app/containers/RandomHabit.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import { AppRegistry, View, TextInput, TouchableOpacity, Text, StyleSheet, Switch } from 'react-native';
 import TimePicker from 'react-native-modal-datetime-picker';
-import styles from './ContainerStyles';
+import { NewHabit } from './ContainerStyles';
 
 var moment = require('moment');
 
-export default class NewHabitContainer extends Component {
+export default class RandomHabit extends Component {
    static navigationOptions = {
     title: "Create a new habit",
   }
@@ -15,17 +15,19 @@ export default class NewHabitContainer extends Component {
     this.state = {
       habitName: null,
       startTime: null,
+      endTime: null,
+      ReminderNumber: null,
       isTimePickerVisible: false,
+      PickerTwoVis: false,
       isTimeChosen: false,
-      ShowInput: false
-      //validInput: false
+      isTime2Chosen: false,
+      ShowInput: false,
+      visible: false,
     };
   }
 
   _submitForm = () => {
-    let habitName = this.state.habitName;
-    let habitTime = this.state.startTime;
-    return fetch('https://habitualdb.herokuapp.com/users/12345/habits', {
+    return fetch('http://localhost:3000/test', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -33,24 +35,19 @@ export default class NewHabitContainer extends Component {
       },
       body: JSON.stringify({
         habit: {
-          name: habitName,
-          reminder_frequency: 1,
-          reminder_time: habitTime
+          name: this.state.habitName,
+          reminder_frequency: this.state.ReminderNumber,
+          reminder_start_time: this.state.startTime,
+          reminder_end_time: this.state.endTime
         }
       })
     })
   }
 
-  // _checkValidInput = () => {
-  //   if (this.state.isTimeChosen && this.state.habitName) {
-
-  //   }
-  // };
-
   _toggleTimePicker = () => this.setState({isTimePickerVisible: !this.state.isTimePickerVisible});
+    _toggleTimePicker2 = () => this.setState({PickerTwoVis: !this.state.PickerTwoVis});
 
   _handleTimePicked = (time) => {
-    this._checkValidInput;
     this.setState({
       isTimePickerVisible: !this.state.isTimePickerVisible,
       isTimeChosen: true,
@@ -58,40 +55,60 @@ export default class NewHabitContainer extends Component {
     });
   };
 
+    _handleTimePicked2 = (time) => {
+    this.setState({
+      PickerTwoVis: !this.state.PickerTwoVis,
+      isTime2Chosen: true,
+      endTime: time
+    });
+  };
+
   render() {
     return (
-      <View style={styles.newHabitView}>
-        <Text style={styles.label}>New Habit</Text>
+      <View>
         <TextInput
-          style={styles.inputBox}
+          style={NewHabit.inputBox}
           placeholder="Habit"
           onChangeText={(habitName) => this.setState({habitName})}
           value={this.state.habitName}
         />
+        <TextInput
+          style={NewHabit.inputBox}
+          keyboardType='numeric'
+          placeholder="Reminders per day"
+          onChangeText={(ReminderNumber) => this.setState({ReminderNumber})}
+          value={this.state.ReminderNumber}
+        />
         <TouchableOpacity onPress={this._toggleTimePicker}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>{this.state.isTimeChosen ? moment(this.state.startTime).format('h:mm a') : "Start Time"}</Text>
+          <View style={NewHabit.button}>
+            <Text style={NewHabit.buttonText}>{this.state.isTimeChosen ? moment(this.state.startTime).format('h:mm a') : "Start Time"}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this._toggleTimePicker2}>
+          <View style={NewHabit.button}>
+            <Text style={NewHabit.buttonText}>{this.state.isTime2Chosen ? moment(this.state.endTime).format('h:mm a') : "End Time"}</Text>
           </View>
         </TouchableOpacity>
         <TimePicker
           isVisible={this.state.isTimePickerVisible}
           mode='time'
-          titleIOS='Choose a start Time'
+          titleIOS='Choose a Start Time'
           onConfirm={this._handleTimePicked}
           onCancel={this._toggleTimePicker}
         />
         <TimePicker
-          isVisible={this.state.isTimePickerVisible}
+          isVisible={this.state.PickerTwoVis}
           mode='time'
-          titleIOS='Choose a Time'
-          onConfirm={this._handleTimePicked}
-          onCancel={this._toggleTimePicker}
+          titleIOS='Choose an End Time'
+          onConfirm={this._handleTimePicked2}
+          onCancel={this._toggleTimePicker2}
         />
+
         <TouchableOpacity
           isVisible={this.state.validInput}
           onPress={this._submitForm}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Submit</Text>
+          <View style={NewHabit.button}>
+            <Text style={NewHabit.buttonText}>Submit</Text>
           </View>
         </TouchableOpacity>
       </View>
